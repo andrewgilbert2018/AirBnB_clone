a258d8fdd019648fddb1039e3e974d50a7829597
Omowunmijuin <balikiskamil@gmail.com>
2023-05-11 09:31:55 -0700

    Class state

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create State class that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class State inherits all the 
+	properties of BaseMode"""
+
+    name = ""
